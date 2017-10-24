d16cf1e4e913624bec5bc2227341107e94018370
David Siroky <david@siroky.cz>
2017-10-24 20:17:54 +0200

    quit command

--- src/backnocles/main.cpp
+++ src/backnocles/main.cpp
@@ -8,5 +8,16 @@
 
 int main(int, char*[])
 {
+  std::cin >> std::ws;
+  while (true)
+  {
+    std::string line;
+    std::getline(std::cin, line);
+    if (line == "quit")
+    {
+      break;
+    }
+  }
+
   return 0;
 }
--- tests/endtoend/test_run.py
+++ tests/endtoend/test_run.py
@@ -5,6 +5,8 @@ Test server run.
 
 import os
 import unittest
+import threading
+import time
 
 from subprocess import Popen, PIPE
 
@@ -16,10 +18,62 @@ SERVER_PROG = os.environ["SERVER_PROG"]
 
 #===========================================================================
 
+class KillAfterTimeout(object):
+    def __init__(self, process, timeout):
+        """
+        @param timeout [s]
+        """
+        self.process = process
+        self.timer = threading.Timer(timeout, self.kill_him)
+        self.timer.start()
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, tp, value, traceback):
+        self.cancel()
+
+    def cancel(self):
+        self.timer.cancel()
+
+    def kill_him(self):
+        try:
+            self.process.terminate()
+        except:
+            pass
+
+#===========================================================================
+
 class TestRun(unittest.TestCase):
-    def TestOutput(self):
+    def TestAwaitsInput(self):
+        """
+        No input - the program will be terminated after a timeout.
+        """
         process = Popen([SERVER_PROG], stdout=PIPE)
-        (output, err) = process.communicate()
-        exit_code = process.wait()
+        tstart = time.time();
+        with KillAfterTimeout(process, 1):
+            (output, err) = process.communicate()
+            exit_code = process.wait()
+        tend = time.time()
+
+        nt.assert_not_equal(exit_code, 0)
+        # killed by timeout
+        nt.assert_greater(tend - tstart, 0.5)
+
+    #----------------------------------------------------
+
+    def TestCommandQuit_ProgramQuits(self):
+        """
+        The program will terminate immediately after issuing the quit command.
+        """
+        process = Popen([SERVER_PROG], stdout=PIPE, stdin=PIPE)
+
+        tstart = time.time();
+        with KillAfterTimeout(process, 1):
+            (output, err) = process.communicate("quit\n")
+            exit_code = process.wait()
+        tend = time.time()
 
         nt.assert_equal(exit_code, 0)
+        # a natural death
+        nt.assert_less(tend - tstart, 0.5)
