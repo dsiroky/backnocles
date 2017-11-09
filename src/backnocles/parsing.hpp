2a34f9d494e7c4d979395c66c37f8a542f2daa61
David Siroky <david@siroky.cz>
2017-11-09 19:10:41 +0100

    parsing: tokens processing

--- src/backnocles/parsing.hpp
+++ src/backnocles/parsing.hpp
@@ -9,6 +9,8 @@
 #include <string_view>
 #include <type_traits>
 
+#include "backnocles/utils/functional.hpp"
+
 //==========================================================================
 namespace backnocles {
 //==========================================================================
@@ -40,6 +42,80 @@ inline void tokenize(const std::string_view input, ForwardIt first)
     new_start = space_pos + 1;
   }
 }
+
+//==========================================================================
+
+/// a "hiding" namespace
+namespace detail::parsing {
+
+/// A wrapper that will use the Factory to produce a particular value and
+/// passes it to the processor.
+template<class Iterator, template<typename> typename Factory, class Processor>
+class ProcessorWrap
+{
+public:
+  ProcessorWrap(Iterator first, Iterator last, Processor& p)
+    : m_first{first}, m_last{last}, m_processor{p}
+  {}
+
+  /// `call<T>() -> Factory<T>::create() -> processor(T)`
+  template<typename T>
+  void call() const
+  {
+    // user might accidentally return a different type other then the
+    // factory template
+    static_assert(
+          std::is_same_v<decltype(Factory<T>::create(m_first, m_last)),
+                          T>
+        );
+
+    m_processor(Factory<T>::create(m_first, m_last));
+  }
+
+private:
+  Iterator m_first, m_last;
+  Processor& m_processor;
+};
+
+} // namespace
+
+//--------------------------------------------------------------------------
+
+/// This class is a "partial specialization" for the Tokens<Factory>::process()
+/// @param Factory a template template where each command has its own specialization.
+///                 For examples see tests::CommandFactory. Each specialization
+///                 must have a static method `call()` with iterator arguments
+///                 `first` and `last` and a return type must be equivalent to
+///                 the Factory specialization.
+template<template<typename> typename Factory>
+struct Tokens
+{
+  /// @param call_map see make_type_call_map()
+  /// @param processor an instance with overloaded `void operator()(const T&)`
+  ///                   methods which will be created with values produced by
+  ///                   the factory.
+  /// @param tokens a list of tokens where the first one is a key to the call
+  ///               map and the rest are raw values for the factory.
+  template<class CallMap, class Processor>
+  static void process(CallMap& call_map,
+                        Processor& processor,
+                        std::initializer_list<std::string_view> tokens)
+  {
+    if (tokens.size() == 0)
+    {
+      throw TypeCallMapNotFound{};
+    }
+    const std::string_view command_name{*tokens.begin()};
+    using ProcessorWrap = detail::parsing::ProcessorWrap<
+                                                decltype(begin(tokens)),
+                                                Factory,
+                                                Processor
+                                              >;
+    call_map.call(command_name,
+                  ProcessorWrap{std::next(begin(tokens)), end(tokens), processor});
+  }
+};
+
 
 //==========================================================================
 } // namespace
--- tests/unit/test_parsing.cpp
+++ tests/unit/test_parsing.cpp
@@ -57,4 +57,154 @@ TEST(Tokenize, MultipleTokens_EquivalentItems)
 }
 
 //==========================================================================
+
+template<typename T>
+struct CommandFactory;
+
+//==========================================================================
+
+/// Command type is "int".
+template<>
+struct CommandFactory<int>
+{
+  template<typename Iterator>
+  static auto create(Iterator, Iterator)
+  {
+    return int{0};
+  }
+};
+
+TEST(ProcessTokens, EmptyTokens_ThrowsException)
+{
+  const auto call_map = backnocles::make_type_call_map(
+                            backnocles::TypeCallMapItem<int>{"dummy"}
+                          );
+  auto processor = [&](const int&){};
+
+  EXPECT_THROW(backnocles::Tokens<CommandFactory>::process(
+                                  call_map,
+                                  processor,
+                                  {}),
+                backnocles::TypeCallMapNotFound);
+}
+
+//--------------------------------------------------------------------------
+
+/// a command type
+struct cmd_quit_t {};
+
+template<>
+struct CommandFactory<cmd_quit_t>
+{
+  template<class Iterator>
+  static auto create(Iterator, Iterator)
+  {
+    return cmd_quit_t{};
+  }
+};
+
+TEST(ProcessTokens, PassQuitToken_CallsQuitCmdOverload)
+{
+  // call flag
+  bool called{false};
+
+  const auto call_map = backnocles::make_type_call_map(
+                            backnocles::TypeCallMapItem<cmd_quit_t>{"quit"}
+                          );
+  auto processor = [&](const cmd_quit_t&){ called = true; };
+
+  backnocles::Tokens<CommandFactory>::process(call_map, processor, {"quit"});
+
+  EXPECT_TRUE(called);
+}
+
+//--------------------------------------------------------------------------
+
+/// @defgroup commandtypes command types
+/// @{
+
+struct cmd_without_params_t {};
+
+struct cmd_with_params_t
+{
+  std::string p1;
+  int p2{0};
+  bool p3{false};
+};
+
+/// @}
+
+template<>
+struct CommandFactory<cmd_without_params_t>
+{
+  template<class Iterator>
+  static auto create(Iterator, Iterator)
+  {
+    return cmd_without_params_t{};
+  }
+};
+
+template<>
+struct CommandFactory<cmd_with_params_t>
+{
+  template<class Iterator>
+  static auto create(Iterator it, Iterator)
+  {
+    return cmd_with_params_t{
+                            std::string{*it++},
+                            std::stoi(std::string{*it++}),
+                            *it == "true"sv
+                          };
+  }
+};
+
+
+TEST(ProcessTokens, PassMultipleTokens_CallsCorrectOverloadWithParameters)
+{
+  // call flags
+  bool called_without_params{false};
+  bool called_with_params{false};
+  cmd_with_params_t created_cmd{};
+
+  const auto call_map = backnocles::make_type_call_map(
+          backnocles::TypeCallMapItem<cmd_without_params_t>{"cmdwithout"},
+          backnocles::TypeCallMapItem<cmd_with_params_t>{"cmdwith"}
+        );
+  auto processor = backnocles::make_overload(
+                [&](const cmd_without_params_t&)
+                {
+                    called_without_params = true;
+                },
+                [&](const cmd_with_params_t& cmd)
+                {
+                    called_with_params = true;
+                    created_cmd = cmd;
+                }
+            );
+
+  backnocles::Tokens<CommandFactory>::process(call_map, processor,
+                              {"cmdwith", "abc", "837", "true"});
+
+  EXPECT_FALSE(called_without_params);
+  EXPECT_TRUE(called_with_params);
+  EXPECT_EQ(created_cmd.p1, "abc"sv);
+  EXPECT_EQ(created_cmd.p2, 837);
+  EXPECT_TRUE(created_cmd.p3);
+}
+
+//--------------------------------------------------------------------------
+
+//TEST(ProcessTokens, ExceptionFromCommandConstructionPropagatesOut)
+//{
+//  // TODO
+//}
+//
+////--------------------------------------------------------------------------
+//
+//TEST(ProcessTokens, ThrowExceptionOnUnknownCommand)
+//{
+//  // TODO
+//}
+
+//==========================================================================
 } // namespace
